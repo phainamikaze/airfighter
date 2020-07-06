9a723375422760b13f3efc2beae2467625c9ccb7
phai@PHAI-OFFICE-PC <phai.namikaze@gmail.com>
2020-07-06 16:01:05 +0700

    03

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ background = pygame.image.load('space.jpg')
 playerImg = pygame.image.load('fighter-jet.png')
 playerX = 375
 playerY = 480
-
+playerX_change = 0
+playerY_change = 0
+player_change_speed = 3
 def player(x,y):
     screen.blit(playerImg,(x,y))
 
@@ -25,6 +27,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = player_change_speed*-1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = player_change_speed
+            if event.key == pygame.K_UP:
+                playerY_change = player_change_speed*-1
+            if event.key == pygame.K_DOWN:
+                playerY_change = player_change_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:    
+                playerY_change = 0
+    playerX += playerX_change
+    playerY += playerY_change
 
     player(playerX,playerY)
 
