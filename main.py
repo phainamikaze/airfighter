f8440c626b49e07f025ccf89addd3c0ccaad6166
phai@PHAI-OFFICE-PC <phai.namikaze@gmail.com>
2020-07-06 16:00:39 +0700

    02

--- main.py
+++ main.py
@@ -5,15 +5,29 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption("Air Fighter")
-icon = pygame.image.load("aircraft.png")
+icon = pygame.image.load("fighter-jet-icon.png")
 pygame.display.set_icon(icon)
+
+background = pygame.image.load('space.jpg')
+
+playerImg = pygame.image.load('fighter-jet.png')
+playerX = 375
+playerY = 480
+
+def player(x,y):
+    screen.blit(playerImg,(x,y))
+
 
 running = True
 while running:
-    screen.fill((255,255,0))
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player(playerX,playerY)
+
 
     pygame.display.update()
     
